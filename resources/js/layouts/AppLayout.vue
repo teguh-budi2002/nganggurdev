77ae0d3adaf82666c526294b6d33928a0d1ee470
teguh-budi2002 <budilaksono1102@gmail.com>
2025-09-04 20:21:45 +0700

    fix bug meta href lang

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,8 +1,8 @@
 <template>
     <Head>
-      <link rel="alternate" hreflang="en" :href="page.props.current_url" />
-      <link rel="alternate" hreflang="id" :href="page.props.current_url" />
-      <link rel="alternate" hreflang="x-default" :href="page.props.current_url" />
+      <link rel="alternate" hreflang="en" :href="page.props.alternate_urls.en" />
+      <link rel="alternate" hreflang="id" :href="page.props.alternate_urls.id" />
+      <link rel="alternate" hreflang="x-default" :href="page.props.alternate_urls.en" />
       <link rel="canonical" :href="page.props.url" />
       <link rel="shortcut icon" href="/assets/images/logo/logo-no-bg.webp" type="image/x-icon">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />

52a1a154a6ae0eda82dd87dc665d62fdcf3ecaf5
teguh-budi2002 <budilaksono1102@gmail.com>
2025-09-04 19:23:31 +0700

    fix meta tag && heading

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,8 +1,8 @@
 <template>
     <Head>
-      <link rel="alternate" hreflang="en" :href="page.props.base_url + '/en'" />
-      <link rel="alternate" hreflang="id" :href="page.props.base_url + '/id'" />
-      <link rel="alternate" hreflang="x-default" :href="page.props.base_url + '/en'" />
+      <link rel="alternate" hreflang="en" :href="page.props.current_url" />
+      <link rel="alternate" hreflang="id" :href="page.props.current_url" />
+      <link rel="alternate" hreflang="x-default" :href="page.props.current_url" />
       <link rel="canonical" :href="page.props.url" />
       <link rel="shortcut icon" href="/assets/images/logo/logo-no-bg.webp" type="image/x-icon">
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
